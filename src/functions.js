16e8f9227c14e21bba55cb0644044a244b3aef21
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-08 10:17:47 -0700

    add spread operator

--- src/functions.js
+++ src/functions.js
@@ -3,7 +3,7 @@ export default ({ name }) => {
 };
 
 export function copyAndPush(arr, x) {
-  const copyArray = arr;
+  const copyArray = [...arr];
   copyArray.push(x);
   return [arr, copyArray];
 }
