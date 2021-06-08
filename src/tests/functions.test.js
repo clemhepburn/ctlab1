55952af0578f61bb8e646f50abfe4a92be830038
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-08 09:35:19 -0700

    add getName test

---
+++ src/tests/functions.test.js
@@ -0,0 +1,7 @@
+describe('getName', () => {
+  it('gets the name', () => {
+    const model = { name: 'ryue', age: 55, job: 'architect', favCar: 'alfa romeo' };
+    const name = getName(model);
+    expect(name).toEqual('ryue');
+  });
+});
