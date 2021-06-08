ca6969962148a0435882112d5b8eb8ff16b96177
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-08 09:58:54 -0700

    create copyAndPush test

--- src/tests/functions.test.js
+++ src/tests/functions.test.js
@@ -1,4 +1,5 @@
 import getName from '../functions.js';
+import copyAndPush from '../functions.js';
 
 describe('getName', () => {
   it('gets the name', () => {
@@ -7,3 +8,12 @@ describe('getName', () => {
     expect(name).toEqual('ryue');
   });
 });
+
+describe('copyAndPush', () => {
+  it('copies the array and pushes a new value', () => {
+    const numbers = [1, 2, 3];
+    const newArray = copyAndPush(numbers, 4);
+    expect(newArray).toEqual([1, 2, 3, 4]);
+  });
+});
+
